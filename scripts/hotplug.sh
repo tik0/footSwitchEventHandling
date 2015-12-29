d376935cdaf7c6e0451aff4f9b8ac1c4073e2e56
Timo Korthals <tkorthals@techfak.uni-bielefeld.de>
2015-12-29 19:32:19 +0100

    Cleanup scripts and add command scripts

--- scripts/hotplug.sh
+++ scripts/hotplug.sh
@@ -1,55 +1,62 @@
 #!/bin/bash
-PROG="/opt/footSwitchEventHandling/footSwitchEventHandling"
+# This script runs the footSwitchEventHandling program
+# for the non-opened RDing event device from /dev/input
+# with the next non-opened scripts in the scripts folder
 
+ROOT="/opt/footSwitchEventHandling"
+SCRIPTS_FOLDER="${ROOT}/scripts/cmd"
+PROG="${ROOT}/footSwitchEventHandling"
+ID_SERIAL="RDing_FootSwitch1F1."
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# Process every event device
 for INP_DEV in $(ls /dev/input/event*); do
-  OUT_STR=$(udevadm info -q all -n ${dev} | grep "ID_SERIAL=RDing_FootSwitch1F1.")
-  if [[ OUT_STR == "" ]]; then
-    continue
-  fi
-  # Check, if the device was opened already
-  OPENED_ALREADY=0
-  for dev_opened in $(ps aux | grep /opt/footSwitchEventHandling | grep -oe "/dev/input/event[0-9]."); do
-    if [[ ${dev_opened} == ${INP_DEV} ]]; then
-      OPENED_ALREADY=1
-    fi
-  done
-  # Start the program (TODO For every device different programs)
-  if [[ ${OPENED_ALREADY} == "0" ]]; then
-    echo "${PROG} --eventDevice ${INP_DEV} --cmdP 'date > /tmp/myDate'" | at now
+  # Check all event devices, if they are a RDing Footswitch, and if so, process it
+  OUT_STR=$(udevadm info -q all -n ${INP_DEV} | grep "ID_SERIAL=${ID_SERIAL}")
+  if [[ ${OUT_STR} == "" ]]; then
+    echo "Dev. ${INP_DEV} is not a RDing device" 1>&2
+  else
+    echo "Process the RDing device at ${INP_DEV}"
+
+    # Check, if the device has been opened by ${PROG} already
+    DEV_OPENED_ALREADY=0
+    for OPEN_DEVICE in $(ps aux | grep ${PROG} | grep -oe "/dev/input/event[0-9]."); do
+      if [[ ${OPEN_DEVICE} == ${INP_DEV} ]]; then
+        echo "Device already opened" 1>&2
+        DEV_OPENED_ALREADY=1
+      fi
+    done
+
+    # Start the program with the next free script
+    # (e.g. 1 is executed, so start the scripts in folder 2)
+    SCRIPT_OPENED_ALREADY=0
+    cmdP=""
+    cmdR=""
+    for SCRIPT_FOLDER in $(ls ${SCRIPTS_FOLDER}); do
+
+      # I need an identifier for the opened scripts.
+      # Thus, I'll put an echo string in front of every command
+      COMMENT_STRING="echo ${SCRIPT_FOLDER}"
+      echo "Process scripts in folder ${SCRIPTS_FOLDER}/${SCRIPT_FOLDER}"
+      # Get the script content
+      cmdP="${COMMENT_STRING}P;$(cat ${SCRIPTS_FOLDER}/${SCRIPT_FOLDER}/P | head -n1)"
+      cmdR="${COMMENT_STRING}R;$(cat ${SCRIPTS_FOLDER}/${SCRIPT_FOLDER}/R | head -n1)"
+      # Search for the "echo" comment string with the regarding script folder number
+      for OPEN_DEVICE in $(ps aux | grep ${PROG} | grep -oe "${COMMENT_STRING}"); do
+          echo "Scripts in ${SCRIPTS_FOLDER}/${SCRIPT_FOLDER} already opened" 1>&2
+          SCRIPT_OPENED_ALREADY=1
+      done
+
+      # Finally, run the program
+      if [[ ${DEV_OPENED_ALREADY} == "0" && ${SCRIPT_OPENED_ALREADY} == "0" ]]; then
+        echo "${PROG} --eventDevice ${INP_DEV} --cmdP \"${cmdP}\" --cmdR \"${cmdR}\"" | at now
+        exit 0
+      fi
+    done
   fi
 done
-
-
-
-# OLD SCRIPT
-## Count the RDing devices
-# OLD_NUM=$(xinput | grep RDing | wc -l)
-# while [[ ${OLD_NUM} == $(xinput | grep RDing | wc -l) ]]; do
-#   echo "OLD_NUM = ${OLD_NUM}"
-#   echo "new = $(xinput | grep RDing | wc -l)"
-#   sleep 1
-# done
-
-## Process every RDing device
-# for dev in $(xinput --list | grep RDing |grep -oe "id=[0-9]." | grep -oe "[0-9]."); do
-#   echo "${dev}"
-#   xinput --disable ${dev}
-#   # Get the input device
-#   INP_DEV=$(xinput --list-props ${dev} | grep -oe "/dev/input/event[0-9].")
-#   # Check, if the device was opened already
-#   OPENED_ALREADY=0
-#   for dev_opened in $(ps aux | grep /opt/footSwitchEventHandling | grep -oe "/dev/input/event[0-9]."); do
-#     if [[ ${dev_opened} == ${INP_DEV} ]]; then
-#       OPENED_ALREADY=1
-#       echo "OPENED_ALREADY"
-#     fi
-#   done
-#   # Start the program (TODO For every device different programs)
-#   if [[ ${OPENED_ALREADY} == "0" ]]; then
-# #     echo "${PROG} --eventDevice ${INP_DEV} --cmdP 'date > /opt/myDate' >/dev/null" | at now
-#     echo ${INP_DEV}
-#     echo "${PROG} --eventDevice ${INP_DEV} --cmdP 'date > /opt/myDate'" | at now
-#   fi
-# done
-# sleep 2
-# wait
+exit 1
